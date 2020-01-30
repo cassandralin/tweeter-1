6d9aaaf7b661bce6b1db5c173b39389961e5619c
cassandralin <cassandralin92@gmail.com>
2020-01-30 13:47:12 +0000

    added cross-site scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -23,14 +23,11 @@ const data = [
   }
 ]
 
-// const renderTweets = function(tweets) {
-// // loops through tweets
-// // calls createTweetElement for each tweet
-// // takes return value and appends it to the tweets container
-// for (let tweet of tweets) {
-//  $(".feed").append(createTweetElement(tweet));
-//   }
-// }
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 const createTweetElement = function(tweet) {
 
@@ -40,13 +37,13 @@ const createTweetElement = function(tweet) {
   let existingTweet = `
   <article class="existing-tweet">
     <div class="existing-tweet-top">
-      <img ${user.avatars} /> 
-      <p>${user.name}</p>
-      <p class="push">${user.handle}</p>
+      <img ${escape(user.avatars)} /> 
+      <p>${escape(user.name)}</p>
+      <p class="push">${escape(user.handle)}</p>
     </div>
     <div class="tweet-body">
       <p>
-        ${content.text}
+        ${escape(content.text)}
       </p>
     </div>
     <footer class="existing-tweet-bottom">
@@ -70,34 +67,60 @@ const renderTweets = function(tweets) {
   }
 }
 
+// $(document).ready(function() {
+// $(".new-tweet form").submit(function(event) {
+//   event.preventDefault();
+//   const $data = $(this).serialize()
+//   if ($data.slice(5) === "" || $data.slice(5) === null) {
+//     alert("Please enter tweet");
+//     return; 
+//   }
+//   // } else ($data.slice(5).length > 140) {
+//   //   alert("Please shorten tweet")
+//   //   return;
+//   // } 
+//   $.ajax({
+//     type: 'POST',
+//     url: "/tweets",
+//     data: $data,
+//     dataType: JSON,
+//     success: loadTweets()
+//   });
+//   // .done(function(data) {
+//   //   loadTweets();
+//   // })
+// });
+// });
 
 $(document).ready(function() {
-$(".new-tweet form").submit(function(event) {
-  event.preventDefault();
-  const $data = $(this).serialize()
-  console.log($(this).value);
-  if ($(".textarea").val() === "" || $(".textarea").val() === null || $(".textarea").val() === undefined ||$(".textarea").val().length > 140){
-    alert("Error");
-    return;
-  } 
-  $.ajax({
-    type: 'POST',
-    url: "/tweets",
-    data: $data,
-    dataType: JSON,
-    success: loadTweets()
-  })
-  // .done(function(data) {
-  //   loadTweets();
-  // })
-});
-});
+  $(".new-tweet form").submit(function(event) {
+    event.preventDefault();
+    const $data = $(this).serialize()
+    if ($data.slice(5) === "" || $data.slice(5) === null || $data.slice(5).length > 140){
+      alert("Error");
+      return;
+    } 
+    $.ajax({
+      type: 'POST',
+      url: "/tweets",
+      data: $data,
+      dataType: JSON,
+      success: loadTweets()
+    })
+    // .done(function(data) {
+    //   loadTweets();
+    // })
+  });
+  });
+
+
 
 const loadTweets = function () {
   $.ajax( "/tweets", {
     method: "GET"
   })
   .then(response => {
+    // (".feed").empty();
     renderTweets(response);
   })
 };
