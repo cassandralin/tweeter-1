6ae44c0e0d20b2336f7e799a7f28feef474a3264
Cassandra <56838251+cassandralin@users.noreply.github.com>
2020-03-10 10:34:36 -0400

    Update client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,199 +1,102 @@
 <<<<<<< HEAD
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const escape =  function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
 
-const createTweetElement = function(tweet) {
-
-  const { user, content, created_at } = tweet;
-  date = new Date(created_at).toUTCString();
-  
-  let existingTweet = `
-  <article class="existing-tweet">
-    <div class="existing-tweet-top">
-      <img ${escape(user.avatars)} /> 
-      <p>${escape(user.name)}</p>
-      <p class="push">${escape(user.handle)}</p>
-    </div>
-    <div class="tweet-body">
-      <p>
-        ${escape(content.text)}
-      </p>
-    </div>
-    <footer class="existing-tweet-bottom">
-      <p>
-        10 days
-      </p> 
-    </footer> `;
-    return existingTweet;
-}
-
-$(document).ready(function() {
-  renderTweets(data);
-})
-
+// push the tweets to append
 const renderTweets = function(tweets) {
-  for (let tweet of tweets) {
-   $(".feed").append(createTweetElement(tweet));
+  let markupArray = [];
+
+  for (const tweet of tweets) {
+    markupArray.push(createTweetElement(tweet));
   }
-}
 
-$(document).ready(function() {
-  $(".toggle").click(function() { // if clicked 
-    $(".new-tweet").slideToggle();
-  })
-  $(".error").hide();
-  $(".errorTwo").hide();
-  $(".new-tweet form").submit(function(event) {
-    event.preventDefault();
-    const $data = $(this).serialize()
-    if ($data.slice(5) === "" || $data.slice(5) === null) {
-      $(".error").slideDown();
-      return;
-    } else if (data.slice(5).length > 140) {
-      $(".errorTwo").slideDown();
-      return;
-    } else {
-    $(".error").slideUp();
-    $(".errorTwo").slideUp();
-    $.ajax({
-      type: 'POST',
-      url: "/tweets",
-      data: $data,
-      dataType: JSON,
-      success: loadTweets()
-    })}
-  });
-  });
-
-const loadTweets = function () {
-  $.ajax( "/tweets", {
-    method: "GET"
-  })
-  .then(response => {
-    $(".feed").empty();
-    renderTweets(response);
-  })
+  let contain = markupArray.reverse().join("");
+  $("#tweets-container").prepend(contain);
 };
 
-loadTweets();
+// mockup of the articles to be replaced
+const createTweetElement = function(data) {
+  const markup = `
+  <article class="tweet"> 
+  <header class="tweetHeader"> 
+    <div class="userInfo">
+      <img class="userAvatar" src="${data.user.avatars}">
+      <p class="userName"> ${data.user.name} </p>
+    </div>
+    <p class="handle"> ${data.user.handle}</p>
+  </header>
+    <div class="actualTweet">
+    ${escape(data.content.text)}
+    </div>
+  <footer>
+    <div class="date"> ${moment(data.created_at).fromNow()} </div>
+    <div class="icons">     
+      <img src="https://image.flaticon.com/icons/svg/2288/2288826.svg" width="20" height="20" align="right">
+      <img src="https://image.flaticon.com/icons/svg/1388/1388946.svg" width="20" height="20" align="right">
+      <img src="https://www.flaticon.com/premium-icon/icons/svg/1549/1549775.svg" width="20" height="20" align="right">
+    </div>
+  </footer>
+</article>
+  `;
+  return markup;
+};
 
-=======
->>>>>>> 1d091a97ecabc98b7f3a22409192330ff0c1bfcd
-
-
-const escape = (str) => {
-  const div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
-
-const createTweetElement = (tweetData) => {
-  const { name, avatar, handle } = tweetData.user;
-  const { text } = tweetData.content;
-  const dateCreated = tweetData.created_at;
-  return `<article class="tweet">
-            <header>
-              <div>
-                <img src="${avatar}" />
-                <span>${name}</span>
-              </div>
-              <span class="tweet-handle">${handle}</span>
-            </header>
-            <section>
-              <span class="tweet-text">${escape(text)}</span>
-            </section>
-            <footer>
-              <span>${(dateCreated)}</span>
-              <div class="tweet-icons">
-                <i class="fas fa-flag"></i>
-                <i class="fas fa-retweet"></i>
-                <i class="fas fa-heart"></i>
-              </div>
-            </footer>
-          </article>`
- };
-
-const renderTweets = (tweets) => {
-  $('#tweets-container').empty();
-  for (const tweet of tweets) {
-    const tweetElement = createTweetElement(tweet);
-    $('#tweets-container').prepend(tweetElement);
-  }
-}
-
-const loadTweets = () => {
-  $.ajax('/tweets', { method: 'GET', dataType: "json" })
-  .then(function (result) {
-    renderTweets(result);
-  });
-}
-
-$(document).ready(() => {
-
-  loadTweets();
-
-  const $form = $('#new-tweet-form');
-  const $text = $('#tweet-area');
-
-  $form.submit((event) => {
-
+// waiting for the document to be ready
+$(document).ready(function() {
+  $(".textForm").submit(function(event) {
+    // alert("Handler for .submit() called.");
     event.preventDefault();
 
-    if ($text.val().length > 140) {
-      $(".error").text("The tweet must be under the character count!");
-      return
-    } else if (!$text.val()) {
-      $(".error").text("Comon, saay sumpin!");
-      return
-    }
+    if($(".textArea").val().length >140 ) {
 
-    $.ajax({
-      url: '/tweets', 
-      method: 'POST',
-      data: $form.serialize(),
-      success:() => {
-        loadTweets();
-        $('#counter').text(140);
-        $('#tweet-area').val('').focus();
-      },
-      error: (error) => {
-        console.log(error);
-      }
+      $('#error').slideToggle("slow")
+      setTimeout(() => {
+        $('#error').slideToggle("slow")
+      },6000)
+
+
+    } else if ($(".textArea").val().length === 0 || $(".textArea").val() === null || $(".textArea").val() === "" ) {
+
+      $('#error').slideToggle("slow")
+      setTimeout(() => {
+        $('#error').slideToggle("slow")
+      },6000)
+      
+    } else {
+
+      $.ajax("/tweets", {
+      method: "POST",
+      data: $(this).serialize()
     })
-  });
+    .then(function(response) {
+      console.log('Success: ', response);
+      console.log($(event).serialize());
+      loadTweets()
+      $(".textArea").val("");
+      $('.counter').text(140);
+    }) 
+  }
+  
+});
 
-  $(".toggle-btn").click(() => {
-    $(".new-tweet").slideToggle("slow");
-    $(".new-tweet textarea").focus();
-  });
-  //The new tweet area is hidden until called upon
-  $(".new-tweet").hide();
-})
+const loadTweets = function() {
+  $.ajax("/tweets", { 
+    method: "GET" 
+  })
+  .then( response => {
+    $('#tweets-container').empty(); 
+    renderTweets(response);
+    })
+  };
+loadTweets();
+
+  $('.toggleArrows').click(function(){
+    $('.new-tweet').slideToggle("slow", function(){
+      console.log("animation COmplete");
+      $('.textArea').focus()
+    })
+  })
+});
